839be2873fdceee1d22a38d6a7728f28057e98c3
sergey shapovalov <Shapovalov.Sergey2016@yandex.ru>
2022-07-05 13:26:10 +0300

    Initial commit

---
+++ src/main/java/com/example/rightCity/RightCityApplication.java
@@ -0,0 +1,13 @@
+package com.example.rightCity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RightCityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RightCityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rightCity/RightCityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rightCity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RightCityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
